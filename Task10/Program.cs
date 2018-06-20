fed03de4975750ce5d229f8f081f5072ab53ec04
Ivan <rovany706@gmail.com>
2018-06-20 13:04:01 +0500

    Add solution

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -3,13 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InputHelper;
 
 namespace Task10
 {
     class Program
     {
+        static void PrintTask()
+        {
+            Console.WriteLine("Задача 10\n=================");
+            Console.WriteLine("Выполнить задание, реализовав динамические структуры данных «вручную»,\n" +
+                              "без использования коллекций языка C#.\n" +
+                              "Создать структуру дерева и написать метод для его уничтожения.\n" +
+                              "=================");
+        }
+
         static void Main(string[] args)
         {
+            PrintTask();
+            int size = Input.ReadInt("Введите количество элементов в дереве: ", 1);
+            TreeNode root = TreeNode.GenerateTree(size);
+            Menu(root);
+        }
+
+        static void Menu(TreeNode root)
+        {
+            Console.Clear();
+            PrintTask();
+            if(root == null)
+                Console.WriteLine("Дерево не создано.");
+            else
+            {
+                Console.WriteLine("Дерево:");
+                TreeNode.ShowTree(root, 5);
+            }
+            Console.WriteLine("1. Создать новое дерево\n2. Добавить элемент в дерево\n" +
+                              "3. Уничтожить дерево\n4. Выйти из программы");
+            int input = Input.ReadInt("Выберите действие: ", 1, 4);
+
+            switch (input)
+            {
+                case 1:
+                    Console.Clear();
+                    int size = Input.ReadInt("Введите количество элементов в дереве: ", 1);
+                    root = TreeNode.GenerateTree(size);
+                    Menu(root);
+                    break;
+                case 2:
+                    Console.Clear();
+                    int number = Input.ReadInt("Введите число, которое хотите жобавить в дерево:");
+                    root = TreeNode.Add(root, number);
+                    Console.WriteLine($"Число {number} добавлено в дерево!");
+                    Console.ReadLine();
+                    Menu(root);
+                    break;
+                case 3:
+                    Console.Clear();
+                    root = TreeNode.DeleteTree(root);
+                    Console.WriteLine("Дерево удалено!");
+                    Console.ReadLine();
+                    Menu(root);
+                    break;
+                case 4:
+                    return;
+            }
         }
     }
 }
---
+++ Task10/TreeNode.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class TreeNode
+    {
+        private static Random rand = new Random();
+        public int Data { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int data)
+        {
+            Data = data;
+        }
+
+        public static TreeNode GenerateTree(int size)
+        {
+            int randomNumber = rand.Next(-50, 51);
+            TreeNode root = new TreeNode(randomNumber);
+            for (int i = 1; i < size; i++)
+                root = Add(root, rand.Next(-50, 51));
+
+            return root;
+        }
+
+        public static TreeNode Add(TreeNode root, int number)
+        {
+            TreeNode p = root;
+            TreeNode r = null;
+            bool ok = false;
+            while (p != null && !ok)
+            {
+                r = p;
+                if (number == p.Data)
+                    ok = true;
+                else if (number < p.Data)
+                    p = p.Left;
+                else
+                    p = p.Right;
+            }
+
+            if (ok)
+                return root;
+            TreeNode newNode = new TreeNode(number);
+
+            if (number < r.Data)
+                r.Left = newNode;
+            else
+                r.Right = newNode;
+            return root;
+        }
+
+        public static void ShowTree(TreeNode p, int l)
+        {
+            if (p != null)
+            {
+                ShowTree(p.Right, l + 5);
+                for (int i = 0; i < l; i++) Console.Write(" ");
+                Console.WriteLine("{0,10:F4} ", p.Data);
+                ShowTree(p.Left, l + 5);
+            }
+        }
+
+        public static TreeNode DeleteTree(TreeNode root)
+        {
+            root = null;
+            return root;
+        }
+    }
+}
